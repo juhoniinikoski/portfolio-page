6e98939b9f2c1015cf643ead29d7d80fe8c482c4
juhoniinikoski <juho.niinikoski@aalto.fi>
2022-02-14 10:57:10 +0200

    footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import HeaderComponent from './HeaderComponent';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 interface Props {
   children: React.ReactNode
@@ -12,7 +13,17 @@ const Layout = (props: Props): JSX.Element => {
       <div className='content'>
         {props.children}
       </div>
-      <footer style={{height: 150}}></footer>
+      <footer style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: 48}}>
+        <div style={{display: 'flex', marginTop: 72, marginBottom: 8}}>
+          <a href={'https://github.com/juhoniinikoski'} target="_blank" rel="noreferrer noopener">
+            <FaGithub color={'#A6A6A6'} size={28} style={{marginRight: 8, cursor: 'pointer'}} />
+          </a>
+          <a href={'https://www.linkedin.com/in/juhoniinikoski/'} target="_blank" rel="noreferrer noopener">
+            <FaLinkedin color={'#A6A6A6'} size={28} style={{marginLeft: 8, cursor: 'pointer'}} />
+          </a>
+        </div>
+        <p style={{fontSize: 17, fontWeight: 300, color: '#a6a6a6'}}>Juho Niinikoski · {new Date().getFullYear()}</p>
+      </footer>
     </div>
   )
 };
